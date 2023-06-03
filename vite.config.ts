9fc644f7e6cf6354a0bc222c7e3cdd92748977b0
MaxSantucci <maksym.marmura@gmail.com>
2023-06-03 16:09:13 +0200

    Added gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,24 +5,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: '/social-network/',
   plugins: [react(), tsconfigPaths()],
-   // resolve: {
-   //    alias: {
-   //       src: 'src',
-   //    },
-   // }
-   // server: {
-   //    proxy: {
-   //       '/api': {
-   //          target: 'https://social-network.samuraijs.com/api/1.0/',
-   //          changeOrigin: true,
-   //          secure: false,
-   //          headers: {
-   //             'Access-Control-Allow-Origin': '*',
-   //             'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-   //             'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
-   //          },
-   //       },
-   //    },
-   // },
 })
