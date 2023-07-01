e577f4ee763485e779eb8786824a32b1f28b9f41
MaxSantucci <maksym.marmura@gmail.com>
2023-07-02 00:37:22 +0200

    test: add test for profile reducer

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {ProfileInfo} from './ProfileInfo/ProfileInfo';
+import { ProfileInfo } from './ProfileInfo/ProfileInfo';
 import ProfileAbout from 'components/Profile/ProfileAbout/ProfileAbout';
 
-export const Profile = () => {
+export const Profile = React.memo(() => {
    return (
-      <div className='bg-gray-100 text-custom' style={{ gridArea: "c" }}>
+      <div className="bg-gray-100 text-custom" style={{ gridArea: 'c' }}>
          <ProfileInfo />
-         <ProfileAbout/>
+         <ProfileAbout />
       </div>
    );
-};
+});
